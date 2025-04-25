2704cb026fb2fcc515db080fba41a782bd4480c0
Rafael Montoya Esquer <rafamontoyaes@gmail.com>
2025-04-24 17:01:06 -0700

    Usar puerto dinámico de Render

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post("/webhook", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log("Servidor escuchando en el puerto", PORT);
 });
